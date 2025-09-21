961daa869a0cab315fd26acb930cc819104b01c2
NidhalRekhaif <mn_rekhaif@esi.dz>
2025-09-21 17:30:20 +0200

    Fixed app and database configuration

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from Publications.routers import publications_router
 from Chercheurs.routers import chercheurs_router
+from contextlib import asynccontextmanager
+from database import init_db,SessionDep
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app = FastAPI()
 
 app.include_router(publications_router, prefix="/publications")
